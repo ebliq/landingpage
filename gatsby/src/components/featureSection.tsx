21c4949f064e5ec5e664adea87215d60664e7b6c
Maximilian Hänsel <mh@maxhaensel.de>
2020-07-08 11:45:33 +0200

    fix merge C

--- gatsby/src/components/featureSection.tsx
+++ gatsby/src/components/featureSection.tsx
@@ -32,7 +32,6 @@ const features = [
   /* <div className=" text-center py-24 bg-primary">    </div> */
 }
 const FeatureSection: any = () => (
-<<<<<<< HEAD
   <section id="feature" className="">
     <div className=" text-center py-24 bg-primary">
       <h1 className="text-white">Das zeichnet uns aus</h1>
@@ -48,12 +47,6 @@ const FeatureSection: any = () => (
           orientation={index}
         />
       ))}
-=======
-  <React.Fragment>
-    <div id="feature" className="px-4 md:px-16 text-center py-12 ">
-      <h1>Das zeichnet uns aus</h1>
-      <p className="text-gray">Give a final call to action because that's what the cool kids are doing.</p>
->>>>>>> d26a9aef1780f05ca1d35b82a0332ae5bd288c6c
     </div>
   </section>
 )
